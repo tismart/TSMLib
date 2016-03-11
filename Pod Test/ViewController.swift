9ebd8b78a713f74eccdbdd42ee5c42bc9373e5be
Franco Castellano <franco.castellano@tismartperu.com>
2016-03-11 18:01:58 -0500

    Initial Commit

---
+++ Pod Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pod Test
+//
+//  Created by Franco Castellano on 3/11/16.
+//  Copyright © 2016 Tismart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
