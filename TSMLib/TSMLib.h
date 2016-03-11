a22e8cdec507453a4be6e6bbf6d67cb3f4599d27
Franco Castellano <franco.castellano@tismartperu.com>
2016-03-11 17:37:18 -0500

    Initial Commit

---
+++ TSMLib/TSMLib.h
@@ -0,0 +1,19 @@
+//
+//  TSMLib.h
+//  TSMLib
+//
+//  Created by Franco Castellano on 3/11/16.
+//  Copyright © 2016 Tismart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSMLib.
+FOUNDATION_EXPORT double TSMLibVersionNumber;
+
+//! Project version string for TSMLib.
+FOUNDATION_EXPORT const unsigned char TSMLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSMLib/PublicHeader.h>
+
+
